ed2405d13f413a7fb2a6f06c0097599a46f080c1
Lucas Borges <lscborges1@gmail.com>
2020-07-06 13:50:00 -0300

    Added Unform integration

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,11 +9,46 @@ interface Inputprops extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<Inputprops> = ({ name, icon, ...rest }) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
-    <TextInput placeholderTextColor="#666360" {...rest} />
-  </Container>
-);
+interface InputValueReference {
+  value: string;
+}
+
+const Input: React.FC<Inputprops> = ({ name, icon, ...rest }) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', error, fieldName } = useField(name);
+  const inputeValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputeValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputeValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputeValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        ref={inputElementRef}
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={(value) => {
+          inputeValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
