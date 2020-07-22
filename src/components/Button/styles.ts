bd0b93ba1a98ec0cb2e1efd029e624ea107d0bf8
Lucas Borges <lscborges1@gmail.com>
2020-07-22 07:37:34 -0300

    Minor Fixes

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
